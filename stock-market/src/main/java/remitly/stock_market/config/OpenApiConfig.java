130b198a9956a868192bb2527cc061712fe13f6c
Konrad <116022112+konrad9743@users.noreply.github.com>
2026-04-28 21:04:14 +0200

    feat: add docker and nginx configuration

---
+++ stock-market/src/main/java/remitly/stock_market/config/OpenApiConfig.java
@@ -0,0 +1,14 @@
+package remitly.stock_market.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        servers = {
+                @Server(url = "/", description = "Default Server URL")
+        }
+)
+public class OpenApiConfig {
+}
