66a2c0705f2a3260143957387d29fadd53ed40e4
Konrad <116022112+konrad9743@users.noreply.github.com>
2026-04-28 21:17:56 +0200

    feat: add proper validation to StockDto

--- stock-market/src/main/java/remitly/stock_market/dto/StockDto.java
+++ stock-market/src/main/java/remitly/stock_market/dto/StockDto.java
@@ -1,3 +1,14 @@
 package remitly.stock_market.dto;
 
-public record StockDto(String name, Integer quantity) {}
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record StockDto(
+        @NotBlank(message = "Stock name cannot be empty")
+        String name,
+
+        @NotNull(message = "Quantity cannot be null")
+        @Min(value = 0, message = "Quantity must be >= 0")
+        Integer quantity
+) {}
