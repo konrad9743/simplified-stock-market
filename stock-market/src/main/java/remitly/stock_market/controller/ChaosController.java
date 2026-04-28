bab3a9d92343b4fe38035d93b728ce419cd4d23d
Konrad <116022112+konrad9743@users.noreply.github.com>
2026-04-28 21:09:55 +0200

    feat: add /post chaos implementation

--- stock-market/src/main/java/remitly/stock_market/controller/ChaosController.java
+++ stock-market/src/main/java/remitly/stock_market/controller/ChaosController.java
@@ -1,12 +1,21 @@
 package remitly.stock_market.controller;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping(path = "/chaos")
 public class ChaosController {
 
     @PostMapping
-    public void chaos() {
+    public ResponseEntity<Void> kill() {
+        CompletableFuture.runAsync(
+                () -> Runtime.getRuntime().halt(1),
+                CompletableFuture.delayedExecutor(100, TimeUnit.MILLISECONDS)
+        );
 
+        return ResponseEntity.ok().build();
     }
 }
