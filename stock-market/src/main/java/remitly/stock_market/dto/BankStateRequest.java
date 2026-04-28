182b256b15014f6f4da15106c0bed2307b403e9c
Konrad <116022112+konrad9743@users.noreply.github.com>
2026-04-28 21:12:25 +0200

    feat: add better validation to BankStateRequest

--- stock-market/src/main/java/remitly/stock_market/dto/BankStateRequest.java
+++ stock-market/src/main/java/remitly/stock_market/dto/BankStateRequest.java
@@ -1,7 +1,10 @@
 package remitly.stock_market.dto;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import java.util.List;
 
-public record BankStateRequest(List<StockDto> stocks) {
-    
-}
+public record BankStateRequest(
+        @NotNull
+        List<@Valid @NotNull StockDto> stocks
+) {}
